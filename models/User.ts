43efd65eccc47a0c75c2a8507942b58fb8429528
samuelch727 <samuelchan727@gmail.com>
2022-03-05 19:26:09 +0800

    added new middleware, create user

--- models/User.ts
+++ models/User.ts
@@ -4,19 +4,22 @@ interface User {
   username: String;
 }
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    match: /^[a-zA-Z0-9]+$/,
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      match: /^[a-zA-Z0-9]+$/,
+    },
+    email: {
+      type: String,
+      required: true,
+      match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      index: true,
+    },
+    password: { type: String, required: true },
   },
-  email: {
-    type: String,
-    required: true,
-    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-    unique: true,
-  },
-  password: { type: String, required: true },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.User || mongoose.model<User>("User", UserSchema);
