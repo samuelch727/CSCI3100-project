cfd7b57acfd714cc7407dacf39eede6575a66c5d
samuelch727 <samuelchan727@gmail.com>
2022-03-05 22:16:59 +0800

    removed api

--- pages/api/_middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-import { NextApiRequest } from "next";
-import jwt from "jsonwebtoken";
-
-export async function middleware(req: NextRequest) {}
-
-// TODO: Auth with JWT
-// TODO: Handle JWT expire
