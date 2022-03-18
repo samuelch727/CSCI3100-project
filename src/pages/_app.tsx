081a56621bc71b8739a61b97b7974388145284a9
samuelch727 <samuelchan727@gmail.com>
2022-03-18 17:35:19 +0800

    added auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Amplify from "aws-amplify";
+import awsconfig from "../aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default withAuthenticator(MyApp);
