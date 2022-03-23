6dd2d25650ac837d749cbfd6b3132d45140252d9
samuelch727 <samuelchan727@gmail.com>
2022-03-23 12:55:11 +0800

    i give them money for this wuwu

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,22 +5,5 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
-    },
-    "auth": {
-        "csci3100project75aea3c4": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "storage": {
-        "s3csci3100projectstorageaededc65": {
-            "BucketName": "string",
-            "Region": "string"
-        }
     }
 }
