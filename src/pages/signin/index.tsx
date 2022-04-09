f03c6579a698a07d757a33e27530dbbdf03385f2
samuelch727 <samuelchan727@gmail.com>
2022-04-09 15:25:10 +0800

    added forgot password and verify password

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -2,9 +2,7 @@
 import Head from "next/head";
 import Amplify, { Auth } from "aws-amplify";
 import awsExports from "../../aws-exports";
-import {
-  Authenticator, Button
-} from "@aws-amplify/ui-react";
+import { Authenticator, Button } from "@aws-amplify/ui-react";
 //import "../project/index.css"
 import { AppProps } from "next/app";
 import { useState, useEffect } from "react";
@@ -18,14 +16,21 @@ import Logo from "next/image";
 Amplify.configure(awsExports);
 
 interface SignInProp {
-    children: any;
+  children: any;
 }
 
 const initialFormState = {
-  username: '', password: '', email: '', name: '', authCode: '', formType: 'signIn'
-}
+  username: "",
+  password: "",
+  verifypassword: "",
+  email: "",
+  name: "",
+  authCode: "",
+  formType: "signIn",
+  resetCode: "",
+};
 
-const SignIn = ({children}: SignInProp) => {
+const SignIn = ({ children }: SignInProp) => {
   // const AccessLoggedInState = () => {
   //   Auth.currentAuthenticatedUser()
   //   .then(() => {
@@ -35,70 +40,123 @@ const SignIn = ({children}: SignInProp) => {
   //     setLoggedIn(false);
   //   })
   // }
-  const router = useRouter()
+  const router = useRouter();
   //const [username, setUsername] = useState('');
   //const [password, setPassword] = useState('');
-  const [formState, updateFormState] = useState(initialFormState)
+  const [formState, updateFormState] = useState(initialFormState);
   const [user, setUser] = useState(null);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   function onChange(e) {
-    e.persist()
-    updateFormState(()=>({...formState, [e.target.name]: e.target.value}))
+    e.persist();
+    updateFormState(() => ({ ...formState, [e.target.name]: e.target.value }));
   }
 
-  const{ formType } = formState
+  const { formType } = formState;
 
   async function signUp() {
-    try {
-      const { username, email, password, name } = formState
-      await Auth.signUp({ username, password, attributes: {email, name}})
-      updateFormState(()=>({...formState, formType: "confirmSignUp"}))
-    } catch(error) {
-      setError(error.toString());
-      console.log('there was an error signing up', error);
+    const { username, email, password, name, verifypassword } = formState;
+    if (password !== verifypassword) {
+      setError("Passwords do not match");
+      return;
     }
-    
+
+    try {
+      await Auth.signUp({ username, password, attributes: { email, name } });
+      updateFormState(() => ({ ...formState, formType: "confirmSignUp" }));
+    } catch (error) {
+      setError(error.toString());
+      console.log("there was an error signing up", error);
+    }
+
     // if (router.asPath === "/signin") router.push("/home");
   }
   async function confirmSignUp() {
     try {
-      const { username, authCode } = formState
-      const userSignUp = await Auth.confirmSignUp(username, authCode)
+      const { username, authCode } = formState;
+      const userSignUp = await Auth.confirmSignUp(username, authCode);
       setUser(userSignUp);
       if (router.asPath === "/signin") router.push("/home");
-    } catch(error) {
+    } catch (error) {
       setError(error.toString());
-      console.log('there was an error', error);
-    }   
+      console.log("there was an error", error);
+    }
   }
 
-  const signIn = async () => {    
-      try {
-        const { username, password } = formState
-        const userLogin = await Auth.signIn(username, password);
-        // onSignIn();
-        setUser(userLogin);
-        if (router.asPath === "/signin") router.push("/home");
-      } catch (error) {
-        setError(error.toString());
-        console.log('there was an error logging in', error);
-      }
-  }
+  const signIn = async () => {
+    try {
+      const { username, password } = formState;
+      const userLogin = await Auth.signIn(username, password);
+      // onSignIn();
+      setUser(userLogin);
+      if (router.asPath === "/signin") router.push("/home");
+    } catch (error) {
+      setError(error.toString());
+      console.log("there was an error logging in", error);
+    }
+  };
 
-    //<div className="fixed inset-0 overflow-y-auto p-4 pt-[25vh]">
+  const forgotPassword = async () => {
+    const { username } = formState;
+    if (!username || username === "") {
+      setError("Username is required");
+      return;
+    }
+    try {
+      await Auth.forgotPassword(username);
+      updateFormState(() => ({ ...formState, formType: "forgotPasswordSent" }));
+    } catch (error) {
+      setError(error.toString());
+      console.log("there was an error", error);
+    }
+  };
+
+  const forgotPasswordSubmit = async () => {
+    console.log("clicked forgot password submit");
+    const { username, resetCode, password, verifypassword } = formState;
+    if (password !== verifypassword) {
+      setError("Passwords do not match");
+      return;
+    }
+    try {
+      await Auth.forgotPasswordSubmit(username, resetCode, password);
+      updateFormState(() => ({ ...formState, formType: "signIn" }));
+    } catch (error) {
+      setError(error.toString());
+      console.log("there was an error", error);
+    }
+  };
+
+  //<div className="fixed inset-0 overflow-y-auto p-4 pt-[25vh]">
   return (
     <div className="main">
-          <div style={{color: "white", backgroundColor: "black", width:"100%"}}> 
-          <p className="text-6xl"><Logo src="/Logo.png" alt="me" width="55" height="50" />Code Code Guide</p></div>
+      <div style={{ color: "white", backgroundColor: "black", width: "100%" }}>
+        <p className="text-6xl">
+          <Logo src="/Logo.png" alt="me" width="55" height="50" />
+          Code Code Guide
+        </p>
+      </div>
       {console.log("formType: ", formType)}
-      { user ? children : <div className="">
-        {
-          formType === 'signIn' && (
+      {user ? (
+        children
+      ) : (
+        <div className="">
+          {formType === "signIn" && (
             <div>
               <h1>Log in your CodeCodeGuide account</h1>
-              <input name="username" onChange={onChange} placeholder="username" /><br />
-              <input name="password" type="password" onChange={onChange} placeholder="password" /><br />
+              <input
+                name="username"
+                onChange={onChange}
+                placeholder="username"
+              />
+              <br />
+              <input
+                name="password"
+                type="password"
+                onChange={onChange}
+                placeholder="password"
+              />
+              <br />
               {/* <TextField 
                 id = 'username'
                 label = 'Username'
@@ -112,54 +170,172 @@ const SignIn = ({children}: SignInProp) => {
                   value = {password}
                   onChange = {e => setPassword(e.target.value)}
                 /> */}
-              <Button onClick={()=>signIn()}>Sign In</Button>
-              <Button id = 'SignUpButton' onClick ={()=> {
-                updateFormState(()=> ({...formState, formType: "signUp"}));
-                setError("");
-                }}>Sign Up</Button>
+              <Button onClick={() => signIn()}>Sign In</Button>
+              <Button
+                id="SignUpButton"
+                onClick={() => {
+                  updateFormState(() => ({ ...formState, formType: "signUp" }));
+                  setError("");
+                }}
+              >
+                Sign Up
+              </Button>
+              <Button
+                onClick={() => {
+                  updateFormState(() => ({
+                    ...formState,
+                    formType: "forgotPassword",
+                  }));
+                  setError("");
+                }}
+              >
+                Forgot Password
+              </Button>
             </div>
-          )
-        }
-        {
-          formType === 'signUp' && (
+          )}
+          {formType === "signUp" && (
             <div>
-              <h1><p className="text-5xl">Log in your CodeCodeGuide account</p></h1>
-              <input name="username" onChange={onChange} placeholder="username" /><br />
-              <input name="password" type="password" onChange={onChange} placeholder="password" /><br />
-              <input name="email" onChange={onChange} placeholder="email" /><br />
+              <h1>
+                <p className="text-5xl">Log in your CodeCodeGuide account</p>
+              </h1>
+              <input
+                name="username"
+                onChange={onChange}
+                placeholder="username"
+              />
+              <br />
+              <input
+                name="password"
+                type="password"
+                onChange={onChange}
+                placeholder="password"
+              />
+              <br />
+              <input
+                name="verifypassword"
+                type="password"
+                onChange={onChange}
+                placeholder="verify password"
+              />
+              <br />
+              <input name="email" onChange={onChange} placeholder="email" />
+              <br />
               <input name="name" onChange={onChange} placeholder="nickname" />
-              <Button onClick={()=>signUp()} onChange={onChange}>Sign Up</Button>
-              <Button id = 'SignInButton' onClick ={()=> {
-                updateFormState(()=> ({...formState, formType: "signIn"}));
-                setError("");
-                }}>Back to Sign In</Button>
+              <Button onClick={() => signUp()} onChange={onChange}>
+                Sign Up
+              </Button>
+              <Button
+                id="SignInButton"
+                onClick={() => {
+                  updateFormState(() => ({ ...formState, formType: "signIn" }));
+                  setError("");
+                }}
+              >
+                Back to Sign In
+              </Button>
             </div>
-          )
-        }
-        {
-          formType === 'confirmSignUp' && (
+          )}
+          {formType === "confirmSignUp" && (
             <div>
-              <input name="authCode" onChange={onChange} placeholder="Confirmation code" />
-              <Button onClick={()=>{
-                confirmSignUp();
-                setError("");
-                }}>Confirm Sign Up
+              <input
+                name="authCode"
+                onChange={onChange}
+                placeholder="Confirmation code"
+              />
+              <Button
+                onClick={() => {
+                  confirmSignUp();
+                  setError("");
+                }}
+              >
+                Confirm Sign Up
               </Button>
-              <Button onClick={()=>{
-                signUp();
-                setError("");
-                }}>Back To Sign Up
+              <Button
+                onClick={() => {
+                  signUp();
+                  setError("");
+                }}
+              >
+                Back To Sign Up
               </Button>
             </div>
-          )
-        }
-        <div>
-          { error != null ? error : any }
+          )}
+          {formType === "forgotPassword" && (
+            <div>
+              <input
+                name="username"
+                onChange={onChange}
+                placeholder="username"
+              />
+              <Button
+                onClick={() => {
+                  forgotPassword();
+                  setError("");
+                }}
+              >
+                Reset password
+              </Button>
+              <Button
+                id="SignInButton"
+                onClick={() => {
+                  updateFormState(() => ({ ...formState, formType: "signIn" }));
+                  setError("");
+                }}
+              >
+                Back to Sign In
+              </Button>
+            </div>
+          )}{" "}
+          {formType === "forgotPasswordSent" && (
+            <div>
+              An reset email had been send to your mailbox
+              <input
+                name="resetCode"
+                onChange={onChange}
+                placeholder="confirmation code"
+              />
+              <input
+                name="password"
+                type="password"
+                onChange={onChange}
+                placeholder="new password"
+              />
+              <br />
+              <input
+                name="verifypassword"
+                type="password"
+                onChange={onChange}
+                placeholder="verify new password"
+              />
+              <Button
+                onClick={() => {
+                  setError("");
+                  forgotPasswordSubmit();
+                }}
+              >
+                Reset password
+              </Button>
+              <Button
+                id="SignInButton"
+                onClick={() => {
+                  updateFormState(() => ({ ...formState, formType: "signIn" }));
+                  setError("");
+                }}
+              >
+                Back to Sign In
+              </Button>
+            </div>
+          )}
+          <div>{error != null ? error : null}</div>
+          <div>
+            <Link href="/">
+              <button>Back to home</button>
+            </Link>
+          </div>
         </div>
-        <div><Link href="/"><button>Back to home</button></Link></div>
-      </div>  
+      )
 
-     /* <TextField 
+      /* <TextField 
        id = 'username'
        label = 'Username'
         value = {username}
@@ -176,9 +352,9 @@ const SignIn = ({children}: SignInProp) => {
       /* <Button id = 'SignUpButton' onClick ={()=> updateFormState(()=> ({
         ...formState, formType: "signUp"
         }))}>Sign Up</Button> */
-    }
+      }
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
