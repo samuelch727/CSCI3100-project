477cd0c73229416a7b03f28de1610461dcb2b20e
samuelch727 <samuelchan727@gmail.com>
2022-04-11 17:22:24 +0800

    removed amplift default login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Amplify from "aws-amplify";
 import awsconfig from "../aws-exports";
-import { Authenticator } from '@aws-amplify/ui-react';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from "@aws-amplify/ui-react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import { API, graphqlOperation } from "aws-amplify";
 import { listProjects } from "../graphql/queries";
-import '@aws-amplify/ui-react/styles.css';
+import "@aws-amplify/ui-react/styles.css";
 import { useEffect } from "react";
 
 Amplify.configure(awsconfig);
@@ -17,17 +17,15 @@ function MyApp({ Component, pageProps, signOut }: any) {
       try {
         const project = await API.graphql(graphqlOperation(listProjects));
         console.log(project);
-      } catch (err) { 
+      } catch (err) {
         console.log(err);
       }
     }
     fetchProject();
-  })
+  });
 
-  return (
-    <Component {...pageProps} signOut={signOut}/>
-  );
+  return <Component {...pageProps} signOut={signOut} />;
   // return <Component {...pageProps} />;
 }
 
-export default withAuthenticator(MyApp);
+export default MyApp;
