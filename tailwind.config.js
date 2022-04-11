c11ec59417fea12083f9b3d64f4a07ddbf786ace
samuelch727 <samuelchan727@gmail.com>
2022-04-11 19:11:26 +0800

    fixed login screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,30 +5,29 @@ module.exports = {
   ],
   theme: {
     screens: {
-      '2xl': {'max': '1535px'},
+      "2xl": { max: "1535px" },
       // => @media (max-width: 1535px) { ... }
 
-      'xl': {'max': '1279px'},
+      xl: { max: "1279px" },
       // => @media (max-width: 1279px) { ... }
 
-      'lg': {'max': '1023px'},
+      lg: { max: "1023px" },
       // => @media (max-width: 1023px) { ... }
 
-      'md': {'max': '767px'},
+      md: { max: "767px" },
       // => @media (max-width: 767px) { ... }
 
-      'sm': {'max': '639px'},
+      sm: { max: "639px" },
       // => @media (max-width: 639px) { ... }
     },
     extend: {
-      colors:{
-        'navbg': '#212228',
-        'navtexttop': '#A8658D',
-        'navtextbottom': '#534F82',
-        'buttoncolor': '#905C4E',
-        'homepagetitle': '#CE9291'
-
-  
+      colors: {
+        navbg: "#212228",
+        navtexttop: "#A8658D",
+        navtextbottom: "#534F82",
+        buttoncolor: "#905C4E",
+        homepagetitle: "#CE9291",
+        inputboxcolor: "#C4C4C4",
       },
     },
   },
